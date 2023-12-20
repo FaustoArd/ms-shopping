c10401426069fd1542e8588be4fff51119128d4c
Fausto Arduino <faustard@gmail.com>
2023-12-20 18:19:51 -0400

    .

--- api-gateway/src/main/java/com/lord/apigateway/configuration/SecurityConfiguration.java
+++ api-gateway/src/main/java/com/lord/apigateway/configuration/SecurityConfiguration.java
@@ -8,14 +8,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtGrantedAuthoritiesConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.ServerAuthenticationConverter;
+import org.springframework.web.server.ServerWebExchange;
 
 
 @Configuration
-@EnableWebFluxSecurity
 public class SecurityConfiguration {
 	
-	@Autowired
+	/*@Autowired
 	private TokenRelayGatewayFilterFactory filterFactory;
 	
 	@Bean
@@ -37,11 +42,20 @@ public class SecurityConfiguration {
 			exchange.pathMatchers("/api/product/**").hasRole("USER");
 			exchange.anyExchange().authenticated();
 			
+			
+			
 		});
+		serverHttp.oauth2Client(oauth2 -> oauth2.);
 	
 		return serverHttp.build();
 	}
 	
-	
-	
+	ReactiveJwtAuthenticationConverter reactiveJwtConverter() {
+		JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+		jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("roles");
+	jwtGrantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+		ReactiveJwtAuthenticationConverter reactiveJwtConverter  = new ReactiveJwtAuthenticationConverter();
+		reactiveJwtConverter.setJwtGrantedAuthoritiesConverter(new ReactiveJwtGrantedAuthoritiesConverterAdapter(jwtGrantedAuthoritiesConverter));
+		return reactiveJwtConverter;
+	*/
 }
