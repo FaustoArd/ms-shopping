6bea5662cd0ba81745157d1a84332da8dcb47f1c
Fausto Arduino <faustard@gmail.com>
2023-12-30 19:30:17 -0400

    .

--- api-gateway/src/main/java/com/lord/apigateway/configuration/SecurityConfiguration.java
+++ api-gateway/src/main/java/com/lord/apigateway/configuration/SecurityConfiguration.java
@@ -3,61 +3,51 @@ package com.lord.apigateway.configuration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
-import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtGrantedAuthoritiesConverterAdapter;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.authentication.ServerAuthenticationConverter;
 import org.springframework.web.server.ServerWebExchange;
 
 import com.lord.apigateway.utils.RSAKeyProperties;
 
 import reactor.core.publisher.Mono;
 
-@Configuration
-@EnableWebFluxSecurity
+
 public class SecurityConfiguration {
 	
-	private final RSAKeyProperties keys;
+	//private final RSAKeyProperties keys;
 	
 	/*@Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
 	private String jwkSetUri;*/
 	
-	public SecurityConfiguration(RSAKeyProperties keys) {
+	/*public SecurityConfiguration(RSAKeyProperties keys) {
 		this.keys = keys;
-	}
+	}*/
 	
-	@Bean
-	SecurityWebFilterChain webFilterChain(ServerHttpSecurity serverHttp) {
+
+	/*SecurityWebFilterChain webFilterChain(ServerHttpSecurity serverHttp) {
 		serverHttp.csrf(csrf -> csrf.disable())
 		.authorizeExchange(exchange -> {
-			exchange.pathMatchers("/eureka/**").permitAll();
-			exchange.pathMatchers("/api/authorization/**").permitAll();
-			exchange.anyExchange().permitAll();
+			exchange.pathMatchers("/eureka/**")
+			.permitAll()
+			.anyExchange()
+			.authenticated();
+			serverHttp.oauth2ResourceServer((oauth2) -> oauth2
+				    .jwt(Customizer.withDefaults())
+				);
+		
+		
 			
 		});
 	
-		/*serverHttp.oauth2ResourceServer(oauth2 -> oauth2
-				.jwt(jwt -> jwt.jwtAuthenticationConverter(reactiveJwtConverter())));*/
+		serverHttp.oauth2ResourceServer(oauth2 -> oauth2
+				.jwt(jwt -> jwt.jwtAuthenticationConverter(reactiveJwtConverter())));
 		
 		
 		return serverHttp.build();
-	}
+	}*/
 	
-	@Bean
+	/*@Bean
 	ReactiveJwtDecoder jwtDecoder() {
 		return NimbusReactiveJwtDecoder.withPublicKey(keys.getPublicKey()).build();
-	}
+	}*/
 	
 	/*@Bean
 	JwtAuthenticationConverter jwtAuthenticationConverter() {
@@ -71,12 +61,12 @@ public class SecurityConfiguration {
 	}*/
 	
 	
-	ReactiveJwtAuthenticationConverter reactiveJwtConverter() {
+	/*ReactiveJwtAuthenticationConverter reactiveJwtConverter() {
 		JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
 		jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("roles");
 	jwtGrantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
 		ReactiveJwtAuthenticationConverter reactiveJwtConverter  = new ReactiveJwtAuthenticationConverter();
 		reactiveJwtConverter.setJwtGrantedAuthoritiesConverter(new ReactiveJwtGrantedAuthoritiesConverterAdapter(jwtGrantedAuthoritiesConverter));
-		return reactiveJwtConverter;
+		return reactiveJwtConverter;*/
 	}
-}
+
--- category-service/target/generated-sources/annotations/com/lord/categoryservice/mapper/CategoryMapperImpl.java
+++ category-service/target/generated-sources/annotations/com/lord/categoryservice/mapper/CategoryMapperImpl.java
@@ -8,7 +8,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-12-27T21:04:04-0400",
+    date = "2023-12-28T15:01:32-0400",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.34.0.v20230523-1233, environment: Java 17.0.7 (Oracle Corporation)"
 )
 public class CategoryMapperImpl implements CategoryMapper {
