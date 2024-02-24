48d605b83905cfaff20a42b920679baa3614e9f7
Fausto Arduino <faustard@gmail.com>
2024-02-24 20:50:36 -0300

    .

--- product-service/target/generated-sources/annotations/com/lord/productservice/mapper/ProductMapperImpl.java
+++ product-service/target/generated-sources/annotations/com/lord/productservice/mapper/ProductMapperImpl.java
@@ -9,7 +9,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-02-24T20:44:41-0300",
+    date = "2024-02-24T20:49:40-0300",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.34.0.v20230523-1233, environment: Java 17.0.7 (Oracle Corporation)"
 )
 public class ProductMapperImpl implements ProductMapper {
