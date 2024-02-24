d4a281760a828514073b32ad5efbf5ca911d63f7
Fausto Arduino <faustard@gmail.com>
2024-02-24 20:46:18 -0300

    .

--- category-service/target/generated-sources/annotations/com/lord/categoryservice/mapper/CategoryMapperImpl.java
+++ category-service/target/generated-sources/annotations/com/lord/categoryservice/mapper/CategoryMapperImpl.java
@@ -8,7 +8,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-02-24T20:39:52-0300",
+    date = "2024-02-24T20:44:40-0300",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.34.0.v20230523-1233, environment: Java 17.0.7 (Oracle Corporation)"
 )
 public class CategoryMapperImpl implements CategoryMapper {
